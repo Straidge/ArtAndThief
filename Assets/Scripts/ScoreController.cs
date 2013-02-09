4f0143b734d08bbb436f090ed0befa8e380aa8c6
DjebbZ <khalid.jebbari@gmail.com>
2013-02-09 23:26:04 +0100

    Display and update score

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ScoreController : MonoBehaviour {
   private int score;
+  private Vector3 position;
+  public GameObject scoreDisplay;
 
   public void addScore(int scoreToAdd) {
     score += scoreToAdd;
@@ -10,9 +12,12 @@ public class ScoreController : MonoBehaviour {
 
   void Start() {
     score = 0;
+    position = new Vector3(0, 100, 0);
+    Instantiate(scoreDisplay, position, Quaternion.identity);
   }
 
   void Update() {
+    GameObject.Find("Score Value").GetComponent<TextMesh>().text = score.ToString();
     Debug.Log("Score : " + score);
   }
 
