d5d58fa88174297c577e00b9f4bacfcc84fecbf0
DjebbZ <khalid.jebbari@gmail.com>
2013-02-09 14:43:20 +0100

    Save splash scene ; add and display separate button

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -6,10 +6,12 @@ public class SplashScreen : MonoBehaviour {
   public Texture backgroundTexture;
   public Rect backgroundArea;
   public Rect mainButtonArea;
+  public Texture mainButtonTexture;
   public GUIStyle mainButtonStyle;
 
   void OnGUI() {
     GUI.DrawTexture(backgroundArea, backgroundTexture);
+    GUI.DrawTexture(mainButtonArea, mainButtonTexture);
     if(GUI.Button(mainButtonArea, "", mainButtonStyle)) {
       Debug.Log("Boutton cliqué");
     }
@@ -17,13 +19,13 @@ public class SplashScreen : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-    float scaleX = 61 / 100f * Screen.width;
-    float scaleY = 61 / 100f * Screen.height;
-    float buttonWidth = 26 / 100f * Screen.width;
-    float buttonHeight = 21 / 100f * Screen.height;
+    float posX = 46 / 100f * Screen.width;
+    float posY = 59 / 100f * Screen.height;
+    float buttonWidth = 39 / 100f * Screen.width;
+    float buttonHeight = 25 / 100f * Screen.height;
 
     backgroundArea = new Rect(0,0, Screen.width, Screen.height);
-    mainButtonArea = new Rect(scaleX, scaleY, buttonWidth, buttonHeight);
+    mainButtonArea = new Rect(posX, posY, buttonWidth, buttonHeight);
 	}
 
 	// Update is called once per frame
